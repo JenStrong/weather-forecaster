fd3e9b70c8e2906403ee0719955e569a7ca516f6
Jen Strong <jenniferastrong@gmail.com>
2018-08-06 18:01:00 +0100

    Add jquery for dynamic background

--- interface.js
+++ interface.js
@@ -1,22 +1,42 @@
 $(document).ready(function(){
+  $("#weather-display").hide();
 
   $('#select-city').submit(function(event){
     event.preventDefault();
-    var city = $('#current-city').val();
-    displayWeather(city);
+    $('#display-city').text($('#current-city').val());
+    $("#greeting").remove();
+    $("#weather-display").show();
+    displayWeather($('#current-city').val());
   })
-
-  $("#get-city").click(function() {
-    var inputCity = $('#current-city').val();
-    $('#display-city').text(inputCity);
-  });
 
   function displayWeather(city){
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
     var token = '&appid=3c4637f8334e84b45a1b5e450fae1ab0';
     var units = '&units=imperial';
+
     $.get(url + token + units, function(data){
       $('#current-temperature').text(data.main.temp);
     })
-  };
+
+    $.get(url + token + units, function(data){
+      $('#weather-description').text(data.weather[0].description);
+      weather_code = (data.weather[0].id)
+
+      if(weather_code === 800){
+        image='url("./public/images/clear.jpg")'
+      }else if(weather_code > 800){
+        image='url("./public/images/clouds.jpg")'
+      }else if(weather_code > 700 && weather_code <= 781){
+        image='url("./public/images/haze.jpg")'
+      }else if(weather_code > 599 && weather_code <= 622){
+        image='url("./public/images/snow.jpg")'
+      }else if(weather_code > 299 && weather_code <= 531){
+        image='url("./public/images/rain.jpg")'
+      }else if(weather_code > 199 && weather_code <= 232){
+        image='url("./public/images/storm.jpg")'
+      }
+      $('body').css('background-image',image)
+    });
+    }
+
 });
